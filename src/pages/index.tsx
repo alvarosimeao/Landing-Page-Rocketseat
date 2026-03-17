56bcb4ad412ead47ca02175d7cf91e9d8c70c8ad
Paulo Souza <dev.souzza.17@gmail.com>
2026-03-17 13:39:52 -0300

    atualizando os baguio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "@/Components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,7 @@ export default function Home() {
   return (
     <div className= "min-h-screen mix-w-full bg-slate-500 ">
       <h2 className= "text=4xl text-red-400">Hello World</h2>
+    <Button className= "text-red-950 bg-lime-600 rounded-4xl w-max h-max rounded-2xl" >Bofaofna</Button>
     </div>
   );
 }
