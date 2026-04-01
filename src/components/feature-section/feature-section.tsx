4dd79ec9d70d3cc0b4f4ae24cb761a384b9433d8
alvaro <alvarosimeao21@gmail.com>
2026-04-01 15:32:33 -0300

    adicionand

--- src/components/feature-section/feature-section.tsx
+++ src/components/feature-section/feature-section.tsx
@@ -10,7 +10,7 @@ export const FeatureSection = () => {
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit  rounded-sm uppercase">
           Simples
         </span>
-        <h2 className="text-gray-100 text-heading-lg">
+        <h2 className="text-gray-100 text-heading-lg font-sans">
           Crie um catálogo de produtos online em poucos minutos
         </h2>
       </div>
@@ -19,7 +19,7 @@ export const FeatureSection = () => {
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit  rounded-sm uppercase">
           Prático
         </span>
-        <h2 className="text-gray-100 text-heading-lg">
+        <h2 className="text-gray-100 text-heading-lg font-sans">
           Venda para seu público através de uma plataforma única
         </h2>
       </div>
@@ -30,12 +30,12 @@ export const FeatureSection = () => {
             <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit  rounded-sm uppercase">
               Personalizável
             </span>
-            <h2 className="text-gray-100 text-heading-lg">
+            <h2 className="text-gray-100 text-heading-lg font-sans">
               Tenha uma loja online personalizada com a cara da sua marca
             </h2>
             <Button
               asChild
-              className="w-fit rounded-full hidden mt-4 md:mt-auto md:flex"
+              className="w-fit rounded-full hidden py-5 px-4 mt-4 md:mt-auto md:flex"
             >
               <Link href="/criar-loja">
                 Criar loja grátis
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from "@/Components/call-to-action";
 import { CustomerStorySection } from "@/Components/customer-story-section";
 import { FeatureSection } from "@/Components/feature-section/feature-section";
 import { HeroSection } from "@/Components/hero-section";
@@ -11,6 +12,7 @@ export default function Home() {
       <FeatureSection/>
       <SupportSection/>
       <CustomerStorySection/>
+      <CallToAction/>
       </article>
     </>
   );
